553166548a303faa36418ba60dc7bdd13b587e0b
Ann Gabr <annabaks1997@gmail.com>
2018-03-14 18:59:02 +0300

    Chenged role attributes. Removed constants and module.

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,22 +1,14 @@
+const hamburger = document.querySelector('[role="trigger"]');
+const popup = document.querySelector('[role="popup"]');
+const popupContainer = document.querySelector('[role="popup-container"]');
+const popupPage = document.querySelector('[role="popup-page"]');
 
-;((window) => {
-    const HAMBURGER = '[hamburger-role="trigger"]';
-    const POPUP = '[popup-role="popup"]';
-    const POPUP_CONTAINER = '[header-role="popup-container"]';
-    const POPUP_PAGE = '[page-role="popup-page"]';
-
-    let hamburger = document.querySelector(HAMBURGER);
-    let popup = document.querySelector(POPUP);
-    let popupContainer = document.querySelector(POPUP_CONTAINER);
-    let popupPage = document.querySelector(POPUP_PAGE);
-
-    if(hamburger && popup && popupContainer && popupPage) {
-        hamburger.addEventListener("click", (event) => {
-            event.preventDefault();
-            hamburger.classList.toggle('is-active');
-            popup.classList.toggle('popup--active');
-            popupContainer.classList.toggle('popup-container--active');
-            popupPage.classList.toggle('page--popup');
-        });
-    }
-})(window);
+if(hamburger && popup && popupContainer && popupPage) {
+    hamburger.addEventListener("click", (event) => {
+        event.preventDefault();
+        hamburger.classList.toggle('is-active');
+        popup.classList.toggle('popup--active');
+        popupContainer.classList.toggle('popup-container--active');
+        popupPage.classList.toggle('page--popup');
+    });
+}
